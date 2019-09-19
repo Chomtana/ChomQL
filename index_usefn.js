2f21cb3f681fb2ac61145d04a8548ac724b8041c
Chomtana <chomtana001@gmail.com>
2019-09-19 20:51:54 +0700

    add use fn style

---
+++ index_usefn.js
@@ -0,0 +1,177 @@
+//utils
+function escapeRegExp(str) {
+	return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+function strReplaceAll(str, find, replace) {
+	return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+function DEFINE_NEW_RES(varname) {
+	return `typeof ${varname} === "object" ? (Array.isArray(${varname}) ? [] : (${varname} === null ? null : {}) ) : ${varname}`
+}
+
+function performSingle(past, key, flexOptions = {}) {
+  let src = past.src;
+	let path = past.path;
+	let newPath = path.concat([key]);
+	
+	//console.log(key);
+	
+  // src = src.replace(
+  //   "/*{{CURRENT}}*/",
+  //   `
+	// 	let data_${newPath.join("_")} = data_${path.join("_")}[${key}];
+	// 	let res_${newPath.join("_")} = ${DEFINE_NEW_RES( "data_"+newPath.join("_") )}
+	// 	/*{{CURRENT}}*/
+	// 	res_${path.join("_")}[${key}] = res_${newPath.join("_")}
+	// `
+	// );
+
+	src = strReplaceAll(src, "/*{{CURRENT}}*/",
+		`
+		{
+			let data_parent = data_current
+			let res_parent = res_current
+			{
+				let data_current = data_parent[${JSON.stringify(key)}];
+				let res_current = ${DEFINE_NEW_RES( "data_current" )}
+				/*{{CURRENT}}*/
+				res_parent[${JSON.stringify(key)}] = res_current;
+			}
+		}
+		`
+	)
+	
+	past.src = src;
+	past.path = newPath;
+	
+	return past;
+}
+
+function performAll(past, flexOptions = {}) {
+  let src = past.src;
+	let path = past.path;
+	let newPath = path.concat(["$"]);
+	
+  //src = src.replace(
+  //  "/*{{CURRENT}}*/",
+	//	`
+	//	for(let key_${newPath.join("_")} in data_${path.join("_")}) {
+	//		let data_${newPath.join("_")} = data_${path.join("_")}[key_${newPath.join("_")}];
+	//		let res_${newPath.join("_")} = ${DEFINE_NEW_RES( "data_"+newPath.join("_") )}
+	//		/*{{CURRENT}}*/
+	//		res_${path.join("_")}[key_${newPath.join("_")}] = res_${newPath.join("_")}
+	//	} 
+	//`
+	//);
+
+	src = strReplaceAll(src, "/*{{CURRENT}}*/",
+		`
+		for(let key_current in data_current) {
+			let data_parent = data_current
+			let res_parent = res_current
+			{
+				let data_current = data_parent[key_current];
+				let res_current = ${DEFINE_NEW_RES( "data_current" )}
+				/*{{CURRENT}}*/
+				res_parent[key_current] = res_current;
+			}
+		}
+		`
+	)
+	
+	past.src = src;
+	past.path = newPath;
+	
+	return past;
+}
+
+function performFinal(past, flexOptions = {}) {
+  let src = past.src;
+	let path = past.path;
+	
+  src = src.replace(
+    "/*{{CURRENT}}*/",
+    `
+		res_current = data_current;
+	`
+	);
+	
+	//console.log(src);
+	
+	past.src = src;
+	past.path.push("$");
+	
+	return past;
+}
+
+function chomQL_proxyHandler(flexOptions = {}) {
+	let proxyHandler = {
+		get(obj, key, receiver) {
+			if (typeof key !== "string" && typeof key !== "number") return;
+			if (key === "inspect") return;
+			
+			if (key == "$") {
+				return buildProxy({
+					$body: performAll( Object.assign({}, Reflect.get(obj, "$body") ), flexOptions)
+				}, proxyHandler);
+			} else if (key[0] == "$") {
+				if (key == "$final") {
+					return buildProxy({
+						$body: performFinal( Object.assign({}, Reflect.get(obj, "$body") ), flexOptions)
+					}, proxyHandler);
+				}
+				return Reflect.get(obj, key)
+			} else {
+				//console.log("Ddsadasd")
+				return buildProxy({
+					$body: performSingle( Object.assign({}, Reflect.get(obj, "$body") ), key, flexOptions)
+				}, proxyHandler);
+			}
+		},
+		apply(____, thisArg, args) {
+			for(let arg of args) {
+				console.log(arg);
+			}
+
+			console.log(Object.assign({}, ____))
+
+			return buildProxy( Object.assign({}, ____) , proxyHandler);
+		}
+	}
+	
+	return proxyHandler;
+}
+
+function buildProxy(obj, proxyHandler) {
+	return new Proxy( Object.assign(function () {}, obj), proxyHandler )
+}
+
+function chomQL(qlfn, flexOptions = {}) {
+	let ql = {
+		$body: {
+			path: [],
+			src: `
+				((obj) => {
+					let res_current = ${DEFINE_NEW_RES("obj")}
+					let data_current = obj
+					/*{{CURRENT}}*/
+					return res_current;
+				})
+			`
+		} //TO DO
+	};
+	
+	let proxy = buildProxy(ql, chomQL_proxyHandler(flexOptions));
+	proxy = qlfn(proxy);
+	proxy = proxy.$final;
+	
+	return proxy;
+}
+
+let a = [[1,8],[2,5],[3,9]];
+let ql = chomQL($=>$.$(5,8));
+console.log(ql)
+//console.log(ql.$body.src)
+//console.log(eval(chomQL($=>$[0].$).$body.src)(a));
